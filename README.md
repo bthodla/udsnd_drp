bb85f02ec419a6cf63080186ca4a6ded23a08237
Bhasker Thodla <bthodla@gmail.com>
2022-04-03 09:56:03 -0400

    Added additional visuals to the web app

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# udsnd_drp
-Udacity Data Science Nanodegree - Disaster Response Piplelines
+# Project GitHub Name: udsnd_drp
+## Project Name: Udacity Data Science Nanodegree - Disaster Response Piplelines Project
+
+In this project, I was provided with two types of data:
+* Messages relating to disaster(s) that people were experiencing
+* Categories of disasters
+
+These two datasets were provided in CSV format.
+
+The objectives of this project are as follows:
+* Load the above data programmatically into Pandas dataframes
+* Merge the two datasets into one
+* Clean the data
+* Load the cleaned data into an SQLITE database
+
+* In another process, load the data from the SQLITE database into Pandas dataframes
+* Split the data into training and test sets
+* Build a text processing pipeline and a machine learning pipeline
+* Train and tune the model using GridSearchCV
+* Output the results of the test set
+* Export the final model as a pickle file
+
+* Finally, run the web
+* app after making sufficient modifications to properly point to the SQLITE database and the saved model file
+
+Code for this process is contained in <Project Root Dir>/data/process.py
+
+Please run this code using the following command:
+
+Usage:
+        python process_data.py <Messages Data File Name> <Categories Data File Name>  <DB File Name>
+Example:
+        python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db
+
+Please be mindful of the sequence of the parameters
--- app/run.py
+++ app/run.py
@@ -1,30 +1,70 @@
+# import libraries
+import sys
+import time
+from collections import Counter
+import pandas as pd
+import numpy as np
 import json
 import plotly
-import pandas as pd
+from plotly.graph_objs import Bar
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-# from sklearn.externals import joblib
 import joblib
 from sqlalchemy import create_engine
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import pickle
+from pprint import pprint
+import re
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sqlalchemy import create_engine
+import warnings
+
+warnings.filterwarnings('ignore')
 
 
 app = Flask(__name__)
 
 def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
+    """
+    A function that accepts a text string and tokenizes the same
+    Specifically, it performs the following operations on the text:
+    * Normalize text by removing anything other than alphabets and numbers
+      and convert to lower case
+    * Tokenize text into a list of words
+    * Remove stopwords using the "english" option
+    * Extract word roots for the words in the word list
+    :param text: (str) source string
+    :return: (list) cleaned words
+    """
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+    # Normalize text by removing anything other than alphabets and numbers
+    # and convert to lower case
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
 
-    return clean_tokens
+    # Tokenize text into a list of words
+    words = word_tokenize(text)
+
+    # Remove stopwords using the "english" option
+    words = [w for w in words if w not in stopwords.words("english")]
+
+    # Extract word roots for the words in the word list
+    words = [WordNetLemmatizer().lemmatize(word, pos='v') for word in words]
+
+    return words
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
@@ -43,7 +83,27 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    # Plotting Categories
+    categories_pct = df[df.columns[4:]].sum() / df.shape[0]
+    categories_pct = categories_pct.sort_values(ascending=False)
+    categories_list = list(categories_pct.index)
+
+    # Word frequency
+    word_freq = []
+    for message in df['message'].values:
+        word_freq.extend(tokenize(message))
+
+    words = []
+    counts = []
+    word_dict = Counter(word_freq)
+    word_list = list(word_dict.most_common(10))
+    for item in word_list:
+        words.append(item[0])
+        counts.append(item[1])
+
+    words_pct = list(np.array(counts) / df.shape[0] * 100)
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -63,10 +123,51 @@ def index():
                 'xaxis': {
                     'title': "Genre"
                 }
+            },
+        },
+        {
+            'data': [
+                Bar(
+                    x=categories_list,
+                    y=categories_pct
+                )
+            ],
+
+            'layout': {
+                'title': 'Messages by Category',
+                'yaxis': {
+                    'title': "Percentage",
+                    'automargin':True
+                },
+                'xaxis': {
+                    'title': "Category",
+                    'tickangle': -90,
+                    'automargin':True
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=words,
+                    y=words_pct
+                )
+            ],
+
+            'layout': {
+                'title': 'Occurrence of top 10 words as a % of all words',
+                'yaxis': {
+                    'title': 'Occurrence',
+                    'automargin': True
+                },
+                'xaxis': {
+                    'title': 'Top 10 words',
+                    'automargin': True
+                }
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
